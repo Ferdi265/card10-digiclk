35b511cc91d30bdacad82e63d9221949723477f8
Ferdinand Bachmann <theferdi265@gmail.com>
2019-08-23 15:06:52 +0200

    factor out modules and set import path

--- __init__.py
+++ __init__.py
@@ -1,81 +1,12 @@
 import os
-import display
+import sys
 import leds
+import display
 import buttons
-import time
 
-def ceilDiv(a, b):
-    return (a + (b - 1)) // b
-
-def tipHeight(w):
-    return ceilDiv(w, 2) - 1
-
-def drawTip(d, x, y, w, c, invert = False, swapAxes = False):
-    h = tipHeight(w)
-    for dy in range(h):
-        for dx in range(dy + 1, w - 1 - dy):
-            px = x + dx
-            py = y + dy if not invert else y + h - 1 - dy
-            if swapAxes:
-                px, py = py, px
-            d.pixel(px, py, col = c)
-
-def drawSeg(d, x, y, w, h, c, swapAxes = False):
-    tip_h = tipHeight(w)
-    body_h = h - 2 * tip_h
-
-    drawTip(d, x, y, w, c, invert = True, swapAxes = swapAxes)
-
-    px1, px2 = x, x + w
-    py1, py2 = y + tip_h, y + tip_h + body_h
-    if swapAxes:
-        px1, px2, py1, py2 = py1, py2, px1, px2
-    d.rect(px1, py1, px2, py2, col = c)
-
-    drawTip(d, x, y + tip_h + body_h, w, c, invert = False, swapAxes = swapAxes)
-
-def drawVSeg(d, x, y, w, l, c):
-    drawSeg(d, x, y, w, l, c)
-
-def drawHSeg(d, x, y, w, l, c):
-    drawSeg(d, y, x, w, l, c, swapAxes = True)
-
-def drawGridSeg(d, x, y, w, l, c, swapAxes = False):
-    sw = w - 2
-    tip_h = tipHeight(sw)
-
-    x = x * w
-    y = y * w
-    l = (l - 1) * w
-    drawSeg(d, x + 1, y + tip_h + 3, sw, l - 3, c, swapAxes = swapAxes)
-
-def drawGridVSeg(d, x, y, w, l, c):
-    drawGridSeg(d, x, y, w, l, c)
-
-def drawGridHSeg(d, x, y, w, l, c):
-    drawGridSeg(d, y, x, w, l, c, swapAxes = True)
-
-def drawGrid(d, x1, y1, x2, y2, w, c):
-    for x in range(x1 * w, x2 * w):
-        for y in range(y1 * w, y2 * w):
-            if x % w == 0 or x % w == w - 1 or y % w == 0 or y % w == w - 1:
-                d.pixel(x, y, col = c)
-
-def drawGrid7Seg(d, x, y, w, segs, c):
-    if segs[0]:
-        drawGridHSeg(d, x, y, w, 4, c)
-    if segs[1]:
-        drawGridVSeg(d, x + 3, y, w, 4, c)
-    if segs[2]:
-        drawGridVSeg(d, x + 3, y + 3, w, 4, c)
-    if segs[3]:
-        drawGridHSeg(d, x, y + 6, w, 4, c)
-    if segs[4]:
-        drawGridVSeg(d, x, y + 3, w, 4, c)
-    if segs[5]:
-        drawGridVSeg(d, x, y, w, 4, c)
-    if segs[6]:
-        drawGridHSeg(d, x, y + 3, w, 4, c)
+sys.path.append('/apps/digiclk/')
+import monotime as utime
+import draw
 
 DIGITS = [
     (True, True, True, True, True, True, False),
@@ -91,12 +22,12 @@ DIGITS = [
 ]
 
 def renderNum(d, num, x):
-    drawGrid7Seg(d, x, 0, 7, DIGITS[num // 10], (255, 255, 255))
-    drawGrid7Seg(d, x + 5, 0, 7, DIGITS[num % 10], (255, 255, 255))
+    draw.Grid7Seg(d, x, 0, 7, DIGITS[num // 10], (255, 255, 255))
+    draw.Grid7Seg(d, x + 5, 0, 7, DIGITS[num % 10], (255, 255, 255))
 
 def renderColon(d):
-    drawGridVSeg(d, 11, 2, 7, 2, (255, 255, 255))
-    drawGridVSeg(d, 11, 4, 7, 2, (255, 255, 255))
+    draw.GridVSeg(d, 11, 2, 7, 2, (255, 255, 255))
+    draw.GridVSeg(d, 11, 4, 7, 2, (255, 255, 255))
 
 def renderText(d, text, blankidx = None):
     bs = bytearray(text)
@@ -110,7 +41,7 @@ def renderBar(d, num):
     d.rect(20, 78, 20 + num * 2, 80, col = (255, 255, 255))
 
 def render(d):
-    ltime = time.localtime()
+    ltime = utime.localtime()
     years = ltime[0]
     months = ltime[1]
     days = ltime[2]
@@ -176,7 +107,7 @@ def checkButton(button, button_long, osbutton, pressed, t):
 def checkButtons():
     global pressed_prev
 
-    t = time.time_monotonic_ms()
+    t = utime.time_monotonic_ms()
     pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.TOP_RIGHT | buttons.BOTTOM_RIGHT)
     cur_buttons = 0
 
@@ -188,9 +119,9 @@ def checkButtons():
     return cur_buttons
 
 def modTime(yrs, mth, day, hrs, mns, sec):
-    ltime = time.localtime()
-    new = time.mktime((ltime[0] + yrs, ltime[1] + mth, ltime[2] + day, ltime[3] + hrs, ltime[4] + mns, ltime[5] + sec, None, None))
-    time.set_time(new)
+    ltime = utime.localtime()
+    new = utime.mktime((ltime[0] + yrs, ltime[1] + mth, ltime[2] + day, ltime[3] + hrs, ltime[4] + mns, ltime[5] + sec, None, None))
+    utime.set_time(new)
 
 def ctrl_display(bs):
     global MODE
---
+++ draw.py
@@ -0,0 +1,72 @@
+def _ceilDiv(a, b):
+    return (a + (b - 1)) // b
+
+def TipHeight(w):
+    return _ceilDiv(w, 2) - 1
+
+def Tip(d, x, y, w, c, invert = False, swapAxes = False):
+    h = TipHeight(w)
+    for dy in range(h):
+        for dx in range(dy + 1, w - 1 - dy):
+            px = x + dx
+            py = y + dy if not invert else y + h - 1 - dy
+            if swapAxes:
+                px, py = py, px
+            d.pixel(px, py, col = c)
+
+def Seg(d, x, y, w, h, c, swapAxes = False):
+    tip_h = TipHeight(w)
+    body_h = h - 2 * tip_h
+
+    Tip(d, x, y, w, c, invert = True, swapAxes = swapAxes)
+
+    px1, px2 = x, x + w
+    py1, py2 = y + tip_h, y + tip_h + body_h
+    if swapAxes:
+        px1, px2, py1, py2 = py1, py2, px1, px2
+    d.rect(px1, py1, px2, py2, col = c)
+
+    Tip(d, x, y + tip_h + body_h, w, c, invert = False, swapAxes = swapAxes)
+
+def VSeg(d, x, y, w, l, c):
+    Seg(d, x, y, w, l, c)
+
+def HSeg(d, x, y, w, l, c):
+    Seg(d, y, x, w, l, c, swapAxes = True)
+
+def GridSeg(d, x, y, w, l, c, swapAxes = False):
+    sw = w - 2
+    tip_h = TipHeight(sw)
+
+    x = x * w
+    y = y * w
+    l = (l - 1) * w
+    Seg(d, x + 1, y + tip_h + 3, sw, l - 3, c, swapAxes = swapAxes)
+
+def GridVSeg(d, x, y, w, l, c):
+    GridSeg(d, x, y, w, l, c)
+
+def GridHSeg(d, x, y, w, l, c):
+    GridSeg(d, y, x, w, l, c, swapAxes = True)
+
+def Grid(d, x1, y1, x2, y2, w, c):
+    for x in range(x1 * w, x2 * w):
+        for y in range(y1 * w, y2 * w):
+            if x % w == 0 or x % w == w - 1 or y % w == 0 or y % w == w - 1:
+                d.pixel(x, y, col = c)
+
+def Grid7Seg(d, x, y, w, segs, c):
+    if segs[0]:
+        GridHSeg(d, x, y, w, 4, c)
+    if segs[1]:
+        GridVSeg(d, x + 3, y, w, 4, c)
+    if segs[2]:
+        GridVSeg(d, x + 3, y + 3, w, 4, c)
+    if segs[3]:
+        GridHSeg(d, x, y + 6, w, 4, c)
+    if segs[4]:
+        GridVSeg(d, x, y + 3, w, 4, c)
+    if segs[5]:
+        GridVSeg(d, x, y, w, 4, c)
+    if segs[6]:
+        GridHSeg(d, x, y + 3, w, 4, c)
